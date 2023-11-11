19cc701497464c7c08f8aa094a260508334d1cb3
Draken1509 <huutrung368@gmail.com>
2023-11-11 18:22:37 +0700

    dangnhap
    code chuc nang dangnhap

--- app/src/main/java/com/example/dangkitiemchung/Enter_Password.java
+++ app/src/main/java/com/example/dangkitiemchung/Enter_Password.java
@@ -2,22 +2,88 @@ package com.example.dangkitiemchung;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
 public class Enter_Password extends AppCompatActivity {
 
+
     TextView txt_password;
     Button btn_login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_password);
-        txt_password =(TextView) findViewById(R.id.txt_password);
-        btn_login=(Button)findViewById(R.id.btn_login);
+        txt_password = (TextView) findViewById(R.id.txt_password);
+        btn_login = (Button) findViewById(R.id.btn_login);
+     btn_login.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+             Intent intent = getIntent();
+             if (intent != null) {
+                 String phoneNumber = intent.getStringExtra("PhoneNumber");
+                 String password = txt_password.getText().toString().trim();
+                 if (phoneNumber != null) {
+                     checkPassword(phoneNumber, password);
+
+                 }
+             }
+         }
+     });
+    }
+
+
+    private void checkPassword(String phoneNumber, String enteredPassword1) {
+
+
+        System.out.println("mat khau :" + enteredPassword1);
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("TaiKhoan");
+        databaseReference.orderByChild("UserName").equalTo(phoneNumber).addListenerForSingleValueEvent(new ValueEventListener() {
+            boolean kt = true;
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    // Số điện thoại tồn tại trong cơ sở dữ liệu
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        String passwordFromDatabase = snapshot.child("PassWord").getValue(String.class);
+                        // Kiểm tra mật khẩu
+                        if (enteredPassword1.equals(passwordFromDatabase)) {
+                            Intent mainIntent = new Intent(Enter_Password.this, MenuMainActivity.class);
+                            mainIntent.putExtra("PhoneNumber_main", phoneNumber);
+                            startActivity(mainIntent);
+                            finish();
+                        }
+                        else {
+                            Toast.makeText(Enter_Password.this, "Mật khẩu không đúng", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Xử lý khi có lỗi truy vấn đến cơ sở dữ liệu
+                Log.e("FirebaseError", databaseError.getMessage());
+            }
+        });
+
+        // Giả định: Trả về false nếu không tìm thấy số điện thoại trong cơ sở dữ liệu
 
     }
+
 }
