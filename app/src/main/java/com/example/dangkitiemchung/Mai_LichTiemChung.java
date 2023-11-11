c99f6ded9461914c1877b1e8475d0dbdc4d17e6a
maimai2k2 <huynhmai01062002@gmail.com>
2023-11-11 14:13:08 +0700

    123
    123

---
+++ app/src/main/java/com/example/dangkitiemchung/Mai_LichTiemChung.java
@@ -0,0 +1,14 @@
+package com.example.dangkitiemchung;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mai_LichTiemChung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mai_lich_tiem_chung);
+    }
+}
