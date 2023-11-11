6bfc6c80c13e63b856fd08eadf1459d46102cf55
maimai2k2 <huynhmai01062002@gmail.com>
2023-11-11 14:24:25 +0700

    commit 2
    commit 2

--- app/src/main/java/com/example/dangkitiemchung/Mai_LichTiemChung.java
+++ app/src/main/java/com/example/dangkitiemchung/Mai_LichTiemChung.java
@@ -1,11 +1,22 @@
 package com.example.dangkitiemchung;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+
 public class Mai_LichTiemChung extends AppCompatActivity {
 
+    TabLayout tabLayout;
+    ArrayList<Fragment> fragmentArrayList = new ArrayList<>();
+    //    String tendg = "";
+//    String madg = "";
+//    DatabaseReference database;
+//    ArrayList<DocGia> list = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
