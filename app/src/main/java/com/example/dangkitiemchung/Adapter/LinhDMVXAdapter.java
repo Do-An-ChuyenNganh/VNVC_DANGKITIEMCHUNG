e50778e95c9fae0bf2b8d0b5915f85064c9cfe67
LinhPhan2k2 <116440170+LinhPhan2k2@users.noreply.github.com>
2023-12-13 22:45:12 +0700

    Update LinhDMVXAdapter.java

--- app/src/main/java/com/example/dangkitiemchung/Adapter/LinhDMVXAdapter.java
+++ app/src/main/java/com/example/dangkitiemchung/Adapter/LinhDMVXAdapter.java
@@ -175,7 +175,7 @@ public class LinhDMVXAdapter extends RecyclerView.Adapter<LinhDMVXAdapter.MyView
                  if (trangthai == false)
                  {
                      //show messagebox
-                     buttonClickListener.onButtonClick("Vaccine đã có trong giỏ hàng");
+                     buttonClickListener.onButtonClick("Vaccine đã thêm vào giỏ hàng");
                      flag = 2;
                      trangthai = true;
                  }
